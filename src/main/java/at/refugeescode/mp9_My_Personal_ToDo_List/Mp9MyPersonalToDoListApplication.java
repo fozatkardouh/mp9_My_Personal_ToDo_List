a7aebac993e14e22611fdac0a2a1e52eee50af05
Fozat Kardouh <fozatkardouh@gmail.com>
2018-04-03 17:52:03 +0200

    added project

---
+++ src/main/java/at/refugeescode/mp9_My_Personal_ToDo_List/Mp9MyPersonalToDoListApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp9_My_Personal_ToDo_List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp9MyPersonalToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp9MyPersonalToDoListApplication.class, args);
+	}
+}
---
+++ src/main/java/at/refugeescode/mp9_My_Personal_ToDo_List/endpoint/Endpoints.java
@@ -0,0 +1,4 @@
+package at.refugeescode.mp9_My_Personal_ToDo_List.endpoint;
+
+public class Endpoints {
+}
